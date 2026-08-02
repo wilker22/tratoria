5ff435ef85fa5a9e3fb02a3b4823bd4308a53d82
wilker22 <wilker-22@outlook.com>
2026-08-02 14:49:10 -0300

    configurações de cadastro de usuário

--- backend/src/controllers/user/CreateUserController.ts
+++ backend/src/controllers/user/CreateUserController.ts
@@ -4,10 +4,14 @@ import { CreateUserService, CreateUserService } from '../../services/user/Create
 class CreateUserController {
     async handle(req: Request, res: Response) {
         const { name, email, password } = req.body;
-        console.log({ name, email, password })
+        
         const createUserService = new CreateUserService();
 
-        const user = await createUserService.execute();
+        const user = await createUserService.execute({
+            name: name,
+            email: email,
+            password: password
+        });
 
         res.json({ message: user })
 
--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,8 +1,14 @@
-class CreateUserService{
-    async execute(){
-        console.log("executando serviço");
+interface CreateUserProps{
+    name: string;
+    email: string;
+    password: string;
+}
 
-        return "Usuário Wilker criado com sucesso !";
+class CreateUserService{
+    async execute({name, email, password}: CreateUserProps){
+        console.log({name, email, password});
+
+        return `Usuário ${name} criado com sucesso!`;
     }
 }
 
